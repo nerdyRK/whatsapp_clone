2d07bbc209f40793253c48fe62e0f6412df6dc65
Ravk <93036264+nerdyRK@users.noreply.github.com>
2024-03-27 12:14:23 +0530

    Update MainArea.jsx to have 100dvh

--- src/components/MainArea.jsx
+++ src/components/MainArea.jsx
@@ -5,7 +5,7 @@ import MaxPart from "./MaxPart";
 function MainArea({ selectedChat }) {
   // console.log(selectedChat);
   return (
-    <div className="main-area h-full overflow-y-hidden flex flex-col">
+    <div className="main-area h-[100dvh] overflow-y-hidden flex flex-col">
       <Ribbon selectedChat={selectedChat} />
       <MaxPart />
     </div>
